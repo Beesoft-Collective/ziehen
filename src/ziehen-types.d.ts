1f83ef85510b61c611d4fd1f5b75ebe57804bbdf
brandontrabon <brandon.trabon@gmail.com>
2023-08-27 00:39:33 +1000

    Early start to the library got the event adding removing code working, created the `isContainer` function, and created the code to process an array of containers and load their children into the setting.

--- src/ziehen-types.d.ts
+++ src/ziehen-types.d.ts
@@ -5,9 +5,27 @@ export interface ContainerOptions {
 export interface ContainerSetting {
   id: string | number;
   container: HTMLElement;
+  items?: HTMLCollection;
   options: ContainerOptions;
 }
 
 export interface GlobalOptions {
   mirrorContainer: HTMLElement;
+  isContainer?: (element: Element) => boolean;
 }
+
+/**
+ * The context is the grabbed item and the container it is grabbed from.
+ */
+export interface GrabbedContext {
+  item: Element;
+  source: HTMLElement;
+}
+
+export type OperationType = 'remove' | 'add';
+export type MouseTypes = 'mousedown' | 'mouseup' | 'mousemove';
+export type TouchTypes = 'touchstart' | 'touchend' | 'touchmove';
+
+export type WithRequiredProperty<Type, Key extends keyof Type> = Type & {
+  [Property in Key]-?: Type[Property];
+};
--- src/ziehen.ts
+++ src/ziehen.ts
@@ -1,9 +1,76 @@
 // this will contain most of the code for the project
-import { ContainerSetting, GlobalOptions } from './ziehen-types';
+import {
+  ContainerSetting,
+  GlobalOptions,
+  GrabbedContext,
+  MouseTypes,
+  OperationType,
+  TouchTypes,
+  WithRequiredProperty
+} from './ziehen-types';
+
+const documentElement = document.documentElement;
 
 const ziehen = (containers: Array<ContainerSetting>, globalOptions: GlobalOptions) => {
-  console.log('containers', containers);
-  console.log('globalOptions', globalOptions);
+  let _grabbed: GrabbedContext;
+
+  const options: WithRequiredProperty<GlobalOptions, 'isContainer'> = {
+    mirrorContainer: document.body,
+    isContainer: globalOptions.isContainer || never,
+  };
+
+  const processContainers = (settings: Array<ContainerSetting>) => {
+    for (let i = 0, length = settings.length; i < length; i++) {
+      const setting = settings[i];
+      setting.items = setting.container.children;
+    }
+  };
+
+  processContainers(containers);
+
+  // the name for the returned dragging object will be geschleppt (what was drake)
+
+  const isContainer = (element: Element) => {
+    const index = containers.findIndex((setting) => setting.container.isSameNode(element));
+    return index > -1 || options.isContainer(element);
+  };
+
+  const registerEvents = (shouldRemove = true) => {
+    const operation: OperationType = shouldRemove ? 'remove' : 'add';
+    registerMouseEvents(operation, 'mousedown', grab);
+  };
+
+  registerEvents();
+
+  const registerMovementEvents = (shouldRemove = true) => {
+    const operation: OperationType = shouldRemove ? 'remove' : 'add';
+  };
+
+  const registerMouseEvents = (operation: OperationType, eventType: MouseTypes, func: (event: MouseEvent | TouchEvent) => void) => {
+    const touch = {
+      mouseup: 'touchend',
+      mousedown: 'touchstart',
+      mousemove: 'touchmove',
+    };
+
+    if (operation === 'add') {
+      document.addEventListener(eventType, func);
+      document.addEventListener(touch[eventType] as TouchTypes, func);
+    } else {
+      document.removeEventListener(eventType, func);
+      document.removeEventListener(touch[eventType] as TouchTypes, func);
+    }
+  };
+
+  const grab = (event: MouseEvent | TouchEvent) => {
+    const mouseEvent = event as MouseEvent;
+    if (mouseEvent.button !== 0 || mouseEvent.metaKey || mouseEvent.ctrlKey) {
+      return;
+    }
+  };
 };
+
+const never = (...args: Array<unknown>) => false;
+const always = (...args: Array<unknown>) => true;
 
 export default ziehen;
